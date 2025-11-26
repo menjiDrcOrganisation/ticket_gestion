e375bd419601965529e031c10351384fa5c0d070
marien <mmanila322@gmail.com>
2025-11-26 17:46:51 +0100

    correction bug

--- resources/views/components/nav-element.blade.php
+++ resources/views/components/nav-element.blade.php
@@ -1,6 +1,7 @@
 
 @php
-$classe = $active
+   $isActive = $active ?? false;
+$classe = $isActive
             ? 'inline-flex items-center py-1 pl-2 bg-blue-200 text-white rounded-xl  border-indigo-400 text-sm font-medium leading-5  focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
             : 'inline-flex items-center py-1 pl-2   border-transparent text-sm font-medium leading-5  hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
 @endphp
--- resources/views/layouts/org.blade.php
+++ resources/views/layouts/org.blade.php
@@ -3,21 +3,23 @@
 <x-nav-element
     title="Tableau de board"
     action="{{ route('dashboard_orginasateur.show') }}"
+    :active="request()->routeIs('dashboard_orginasateur.show')"
 >
 </x-nav-element>
 
-<x-nav-element title="Info billets" action="{{ route('billet.all') }}">
+<x-nav-element title="Info billets" :active="request()->routeIs('billet.all')" action="{{ route('billet.all') }}">
 </x-nav-element>
 
-<x-nav-element title="Scanner" action="{{ route('scanneur.showScanner') }}">
+<x-nav-element title="Scanner" :active="request()->routeIs('scanneur.showScanner')" action="{{ route('scanneur.showScanner') }}">
 </x-nav-element>
 
-<x-nav-element title="Demande retrait" action="{{ route('retrait.index') }}">
+<x-nav-element title="Demande retrait" :active="request()->routeIs('retrait.index')" action="{{ route('retrait.index') }}">
 </x-nav-element>
 
 <x-nav-element
     title="Événement scanner"
     action="{{ route('event_scanner.index') }}"
+    :active="request()->routeIs('event_scanner.index')"
 >
 </x-nav-element>
 
