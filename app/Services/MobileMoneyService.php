f78cc8f5cb3d848ebd59d14fd7f5ddb3a9c752b0
marien <mmanila322@gmail.com>
2026-03-29 12:12:09 +0200

    corection

--- app/Services/MobileMoneyService.php
+++ app/Services/MobileMoneyService.php
@@ -59,9 +59,9 @@ class MobileMoneyService
 
             $payload = [
                 'transactionReference' => $data['transactionReference'],
-                'gatewayMode' => "0",
-                'publicApiKey' => env('MAISHAPAY_PUBLIC_KEY_TEST'),
-                'secretApiKey' => env('MAISHAPAY_SECRET_KEY_TEST'),
+                'gatewayMode' => "1",
+                'publicApiKey' => env('MAISHAPAY_PUBLIC_KEY_PROD'),
+                'secretApiKey' => env('MAISHAPAY_SECRET_KEY_PROD'),
                 'order' => [
                     'amount' => $data['amount'],
                     'currency' => $data['currency'],
