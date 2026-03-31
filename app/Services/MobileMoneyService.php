181484a34924b7d56b4eb023376c83cc9885d3b3
marien <mmanila322@gmail.com>
2026-03-31 03:00:09 +0200

    perf(billet):telechargement billet

--- app/Services/MobileMoneyService.php
+++ app/Services/MobileMoneyService.php
@@ -54,9 +54,9 @@ public static function sendPayment($request)
 
         $payload = [
             'transactionReference' => $transactionReference,
-            'gatewayMode' => "1",
-            'publicApiKey' => env('MAISHAPAY_PUBLIC_KEY_PROD'),
-            'secretApiKey' => env('MAISHAPAY_SECRET_KEY_PROD'),
+            'gatewayMode' => "0",
+            'publicApiKey' => env('MAISHAPAY_PUBLIC_KEY_TEST'),
+            'secretApiKey' => env('MAISHAPAY_SECRET_KEY_TEST'),
             'order' => [
                 'amount' => $total,
                 'currency' => $request['devise'],
