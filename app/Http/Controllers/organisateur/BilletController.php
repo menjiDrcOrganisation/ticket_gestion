ae5e166f6acc2b40d8386c257e98a111481f0071
marien <mmanila322@gmail.com>
2026-04-17 17:09:59 +0200

    feat:correction

--- app/Http/Controllers/organisateur/BilletController.php
+++ app/Http/Controllers/organisateur/BilletController.php
@@ -108,14 +108,18 @@ class BilletController extends Controller
         $devise = $billet->type_billet->devise;
         $total = $prix * $billet->quantite;
         
-        $qrImage = base64_encode(
-            QrCode::format('svg')->size(300)->generate($billet->code_billet)
-        );
+        $qrSvg = QrCode::format('svg')
+            ->size(300)
+            ->generate($billet->code_billet);
 
+        // encoder proprement
+        $qrImage = base64_encode($qrSvg);
+
+        // créer l’URL base64
         $qrCodeUrl = 'data:image/svg+xml;base64,' . $qrImage;
 
-
-
+        // passer à la vue
+        
         $data = [
             'ticket' => [
                 'user_name' => $billet->nom_auteur,
